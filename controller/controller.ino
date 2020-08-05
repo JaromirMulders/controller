0b623f384ebc8162e03e1f87d3e4571e57e5ee15
jaromirmulders <jaro92@hotmail.com>
2020-08-05 20:13:52 +0200

    got evereything working(sort of)

--- controller/controller.ino
+++ controller/controller.ino
@@ -15,9 +15,10 @@ float potVal[N_POTS/2];
 float oldVal[N_POTS/2];
 float history[N_POTS/2];
 int   sendVal[N_POTS/2];
+int   oldSendVal[N_POTS/2];
 int   ledOrder[N_POTS] =   {6 ,4 ,2 ,0, 7,5,3,1 ,8,10,12,14,9,11,13,15};
 int   ledOffest[N_POTS] =  {11,11,11,11,5,5,5,11,0,0 ,11,0 ,5,5 ,4, 5};
-
+byte  extLedVal[N_POTS/2];
  
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
  
@@ -29,16 +30,32 @@ void setup() {
     potVal[i/2] = 0.0;
     history[i/2] = 0.0;
     sendVal[i/2] = 0;
-   
+    oldSendVal[i/2] = 0;
+    extLedVal[i/2] = 0;
   }//for
 
-  Serial.begin(9600);
-  //Serial2.begin(9600);
+  //Serial.begin(9600);
+  Serial2.begin(9600);
   strip.begin();
   
 }//setup
  
 void loop() {
+
+  
+  if (Serial2.available() > 0) {  
+    static byte pot_id = 0;
+    static byte bCount = 0;
+  
+    byte incomingbyte = Serial2.read();
+
+    if(incomingbyte >= 200){
+      pot_id = incomingbyte-200;
+    }else if (incomingbyte > -1){
+      extLedVal[pot_id] = incomingbyte;
+    }//if
+    
+  }//if
   
   for(int i = 0; i < N_POTS/2; i++){
     int iter_1 = i*2;
@@ -46,77 +63,100 @@ void loop() {
     
     rawPotVal[iter_1] = analogRead(potId[iter_1]);
     rawPotVal[iter_2] = analogRead(potId[iter_2]);    
+    
+    float fRawVal1 = (float)rawPotVal[iter_1]/512.0 - 1.0;
+    float fRawVal2 = (float)rawPotVal[iter_2]/512.0 - 1.0;
 
-    //first scale potvalues from 0-1024. to -1.-1.
+    //first scale potvalues from 0-256 to -1.-1.
     //then calculate angle for potmeter
-    float val = atan2(rawPotVal[iter_1] /512.-1.,rawPotVal[iter_2] /512.-1.);
+    float val = atan2(fRawVal1,fRawVal2);
     //subtract form oldval to get delta of potmeter
-    float newVal  = constrain(val-oldVal[i],-0.5,0.5)*1000.;
+    float newVal  = constrain(val-oldVal[i],-0.5,0.5)*800.;
     oldVal[i] = val;
 
+    
     //filter with onepole filter to remove noise
-    float filter_C = 0.025;
-    val = history[i] * (1. - filter_C) + newVal * filter_C;
+    float filter_C = 0.5;
+    newVal = history[i] * (1. - filter_C) + newVal * filter_C;
     history[i] = newVal;
-
+    
+    
     //add delta to currentvalue to get new value
     potVal[i]+=newVal;
     potVal[i]= constrain(potVal[i],0.0,8192.0);
     sendVal[i] = (int)potVal[i];
 
-    //Serial.print(sendVal[i]);
-    //Serial.print(" ");
  
   }//for
-  //Serial.println();
 
   //send serial data to teensy
-  sendSerial(sendVal,sizeof(sendVal));
+  sendSerial(sendVal,oldSendVal,sizeof(sendVal)/sizeof(sendVal[0]));
+  
 
   /*turn on leds
   this for loops turns on the current led and turns off the led next to it
   this way i don't have to loop trough all the leds to check which led
-  have to be on or off
-  */
+  has to be on or off
+   */
+
   for(int i = 0; i < N_POTS/2; i++){
     
-    uint8_t lo = ledOrder[i+8]*N_LEDPR;
+    uint8_t lo_1 = ledOrder[i+8]*N_LEDPR;
+    uint8_t lo_2 = ledOrder[i]*N_LEDPR;
+    
     //divide potvals from 0-8192 to 0 - N_LEDPR 
     uint8_t potDivide = sendVal[i]/683;
     //Add offset for ledringId
-    uint8_t ledVal = ledOffest[i+8]+potDivide;
+    uint8_t ledVal_1 = ledOffest[i+8]+potDivide;
+    uint8_t ledVal_2 = ledOffest[i]+extLedVal[i];
+    
     //calculate where next led needs to be
     //the % is beceasue of the circular leds
-    uint8_t ledId  = (ledVal%N_LEDPR) + lo;
-    //calculate where next led the needs to be off needs to be
-    uint8_t ledOffId  = ((ledVal+1)%N_LEDPR) + lo;
+    uint8_t ledId_1  = (ledVal_1%N_LEDPR) + lo_1;
+    uint8_t ledId_2  = (ledVal_2%N_LEDPR) + lo_2;
     
-    showLedDial(ledId,20*(potDivide+1),10,10);
+    //calculate where next led the needs to be off needs to be
+    uint8_t ledOffId_1  = ((ledVal_1+1)%N_LEDPR) + lo_1;
+    uint8_t ledOffId_2  = ((ledVal_2+1)%N_LEDPR) + lo_2;
+    
+    showLedDial(ledId_1,20,10,10);
+    showLedDial(ledId_2,10,10,20);
 
     //don't turn off leds when the potDivide is N_LEDPR so ledring can be fully lit
     if(potDivide < N_LEDPR-1){
-      showLedDial(ledOffId,0,0,0);    
+      showLedDial(ledOffId_1,0,0,0);   
+    }//if
+    if(extLedVal[i] < N_LEDPR-1){
+      showLedDial(ledOffId_2,0,0,0);   
     }//if
     
   }//for
+  
   strip.show();  
-
-
+  
+ 
 }//loop
 
-void sendSerial(int * sendArray,int sizeOfArray){
+void sendSerial(int * sendArray,int * oldSendArray ,int sizeOfArray){
   
-  //send id of controllerboard
-  Serial2.write(200);
-
   //15 = 11110000
   byte bitMask = 15;
   
-  for(int i = 0; i < sizeOfArray; i++){
-    Serial2.write(sendArray[i]       &bitMask);
-    Serial2.write(sendArray[i] >> 4  &bitMask);
-    Serial2.write(sendArray[i] >> 8  &bitMask);
-    Serial2.write(sendArray[i] >> 12 &bitMask);
+  for(int i = 0; i < sizeOfArray; i++){    
+    //send serial data if data has changed
+    if(sendArray[i] != oldSendArray[i]){
+      
+      //send id of potmeter in last 4 byts
+      Serial2.write(200+i);
+      //send data of potmeter in first 4 bytes
+      Serial2.write(sendArray[i]       &bitMask);
+      Serial2.write(sendArray[i] >> 4  &bitMask);
+      Serial2.write(sendArray[i] >> 8  &bitMask);
+      Serial2.write(sendArray[i] >> 12 &bitMask);
+           
+    }//if
+    
+    oldSendArray[i] = sendArray[i];        
   }//for
   
 }//sendSerial
@@ -126,16 +166,6 @@ void showLedDial(uint8_t id, uint8_t r, uint8_t g, uint8_t b){
   strip.setPixelColor(id,r,g,b);
 }//showLedDial
  
-static void chase(uint32_t c) {
-
-  for(uint16_t i=0; i<strip.numPixels()+4; i++) {
-      strip.setPixelColor(i  , c); // Draw new pixel
-      strip.setPixelColor(i-4, 0); // Erase pixel a few steps back
-      strip.show();
-      delay(25);
-  }//for
-  
-}//chase
 
 //function for converting an array of bits to a byte
 byte bitsToByte(bool * bitArray){
--- controllerMidi/controllerMidi.ino
+++ controllerMidi/controllerMidi.ino
@@ -2,17 +2,23 @@
 #define A_RES   1024
 #define C_PI    3.14159265359
 #define C_TWOPI 6.28318530718
-#define C_CCSTART 60
+#define C_CCUPSTART 60
+#define C_CCDOWNSTART 10
 #define C_MIDICHANNEL 1
+#define N_BYTES 4
 
 uint8_t potId[N_POTS] = {A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15};
 
 int     rawPotVal[N_POTS]; 
 float   potVal[N_POTS/2]; 
 float   oldVal[N_POTS/2];
-float   history[N_POTS/2];
+float   history[N_POTS];
 int     sendVal[N_POTS/2];
 int     oldSendVal[N_POTS/2];
+int     oldSerVal[N_POTS/2];
+
+
+byte    receiveBytes[N_POTS/2][N_POTS/2 * N_BYTES];
 
 void setup() {
 
@@ -25,35 +31,76 @@ void setup() {
     oldSendVal[i/2] = 0;
   }//for
 
+  //turn on led on teensy board
   pinMode(13,OUTPUT);
-  
   digitalWrite(13,HIGH);
+
+  analogReference(EXTERNAL);
   
-  Serial.begin(9600);
-  Serial3.begin(9600);
+  Serial1.begin(9600);
+  //Serial2.begin(9600);
+
 }
 
 void loop() {
 
+  //check if arduino mega is sending data else check own data
+  if (Serial1.available() > 0) {
+  //get potmeter data from arduino mega
+    getSerial(); 
+  }else{
+    checkPots();
+  }
+
+}//loop
+
+void getSerial(){
+  static byte pot_id = 0;
+  static byte bCount = 0;
   
-  for(int i = 0; i < N_POTS/2; i++){
+  //if there is Serial data availible 
+  //if (Serial1.available() > 0) {
+    byte incomingbyte = Serial1.read();
+    if(incomingbyte >= 200){
+      pot_id = incomingbyte-200;
+      bCount = 0;
+    }else if(incomingbyte > -1){
+      byte bytePos = pot_id*N_BYTES + bCount;
+      //receiveBytes[pot_id][bytePos] = incomingbyte;
+
+      //send received bytes trough to usbMidi
+      usbMIDI.sendControlChange(C_CCDOWNSTART + bytePos ,incomingbyte, C_MIDICHANNEL);
+
+      bCount++;
+    }
+  //}//if
+  
+}//getSerial
+
+void checkPots(){
+
+ for(int i = 0; i < N_POTS/2; i++){
     int iter_1 = i*2;
     int iter_2 = iter_1 + 1;
-    
+
     rawPotVal[iter_1] = analogRead(potId[iter_1]);
     rawPotVal[iter_2] = analogRead(potId[iter_2]);    
 
-    //first scale potvalues from 0-1024. to -1.-1
-    //then calculate angle for potmeter
-    float val = atan2((float)rawPotVal[iter_1] /512. -1.,(float)rawPotVal[iter_2] / 512. -1.);
+    //let value go from -1. 1.
+    float fRawVal1 = (float)rawPotVal[iter_1]/512.0 - 1.0;
+    float fRawVal2 = (float)rawPotVal[iter_2]/512.0 - 1.0;
+
+    //calculate angle for potmeter
+    float val = atan2(fRawVal1,fRawVal2);
     //subtract form oldval to get direction of potmeter
     float newVal  = constrain(val-oldVal[i],-0.5,0.5)*1000.;
     oldVal[i] = val;
-
+    
     //filter with onepole filter to remove noise
-    float filter_C = 0.025  ;
+    float filter_C = 0.025;
     newVal = history[i] * (1. - filter_C) + newVal * filter_C;
     history[i] = newVal;
+    
 
     //add direction to currentvalue to get new value
     potVal[i]+=newVal;
@@ -68,29 +115,29 @@ void loop() {
       int sendVal3 = sendVal[i]>>8  & 15;
       int sendVal4 = sendVal[i]>>12 & 15;  
           
-      usbMIDI.sendControlChange(C_CCSTART+i*4  , sendVal1, C_MIDICHANNEL);
-      usbMIDI.sendControlChange(C_CCSTART+i*4+1, sendVal2, C_MIDICHANNEL);
-      usbMIDI.sendControlChange(C_CCSTART+i*4+2, sendVal3, C_MIDICHANNEL);
-      usbMIDI.sendControlChange(C_CCSTART+i*4+3, sendVal4, C_MIDICHANNEL);  
+      usbMIDI.sendControlChange(C_CCUPSTART+i*4  , sendVal1, C_MIDICHANNEL);
+      usbMIDI.sendControlChange(C_CCUPSTART+i*4+1, sendVal2, C_MIDICHANNEL);
+      usbMIDI.sendControlChange(C_CCUPSTART+i*4+2, sendVal3, C_MIDICHANNEL);
+      usbMIDI.sendControlChange(C_CCUPSTART+i*4+3, sendVal4, C_MIDICHANNEL);  
+
+      byte serSendVal = sendVal[i]/683;
+        if(serSendVal != oldSerVal[i]){
+        //send value to arduino mega for setting the leds first value is the id of the pot
+        //second value is the led value of the pot
+        Serial1.write(200+i);
+        Serial1.write(serSendVal);
+        Serial.print(200+i);
+        Serial.print(" ");
+        Serial.print(serSendVal);
+        Serial.print(" ");
+   Serial.println();
+       
+                
+      }
+      oldSerVal[i] = serSendVal;
+
     }//if
     oldSendVal[i] = sendVal[i];
-
-    //Serial.print(sendVal[i]);
-    //Serial.print(" ");
-
+    
   }//for
-  //Serial.println();
-
-  getSerial();
-
-}
-
-void getSerial(){
-  //if there is Serial data availible 
-  if (Serial3.available() > 0) {
-    byte incomingbyte = Serial3.read();
-    Serial.println(incomingbyte);
-
-  }//if
-
-}//getSerial
+}//checkpots
